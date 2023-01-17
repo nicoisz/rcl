e74a783363c2425d09378bdb008482a9ae58b7a3
nicoisz <nicolasignacio.sz@gmail.com>
2023-01-17 14:18:35 -0500

    end of game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,18 @@ export class AppComponent {
   dice_center = "./assets/dice_c.png";
   dice_left = "./assets/dice_l.png";
   rotate:boolean = false;
+  visible:boolean = true;
+  numberOfDice:number=0;
+  blockThrowDice:boolean = false;
+  visible_r:boolean=true;
+  visible_c:boolean=true;
+  visible_l:boolean=true;
+  visible_seccion:boolean = true;
 
   throwDice(): void {
 
     this.rotate = true;
+    this.blockThrowDice = false;
 
     setTimeout(() => {
       let number_dice_one = Math.round(Math.random() * 5) + 1;
@@ -24,6 +32,7 @@ export class AppComponent {
       this.selectDiceCenter(number_dice_two);
       this.selectDiceLeft(number_dice_three)
       this.rotate = false;
+      this.visible_seccion = true;
     }, 4000);
 
   }
@@ -68,5 +77,23 @@ export class AppComponent {
     } else if(number === 6){
       this.dice_left = "./assets/dice_w.png";
     }
+  }
+
+  quantityDice(quantity:number):void{
+    
+    this.numberOfDice = quantity;
+    this.visible_seccion = false;
+    if(this.numberOfDice == 1){
+      this.visible_r = false;
+      this.visible_l = false;
+    } else if(this.numberOfDice == 2){
+      this.visible_r = true;
+      this.visible_l = false;
+    }else{
+      this.visible_r = true;
+      this.visible_l = true;
+    }
+
+    this.blockThrowDice = true;
   }
 }
